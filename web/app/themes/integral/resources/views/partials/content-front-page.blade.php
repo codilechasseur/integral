a7737c7a02d624bd1cbbfea5fc705d4f7bb25c6d
Codi Lechasseur <codilechasseur@gmail.com>
2024-01-09 15:48:15 -0800

    Progress.

--- web/app/themes/integral/resources/views/partials/content-front-page.blade.php
+++ web/app/themes/integral/resources/views/partials/content-front-page.blade.php
@@ -22,7 +22,7 @@
 </div>
 
 <div class="bg-dust flex flex-col lg:flex-row">
-  <div class="bg-dark-gray lg:w-2/5 px-4 md:px-8 lg:px-16">
+  <div class="lg:bg-gradient-to-br from-dark-gray to-charcoal lg:w-2/5 px-4 md:px-8 lg:px-16">
     <h3 class="font-serif text-medium-gray text-9xl md:text-11xl lg:text-12xl leading-4 tracking-wider">Unlocking <em class="block text-white">the Potential</em> in Real Estate</h3>
     <p class="text-white text-lg md:text-xl lg:text-3xl leading-10 tracking-tight font-light">Managing real estate investments today has never been more complex and the risks have never been higher. Our proven strategic approach, rooted in financial due diligence and creative problem solving delivers long-term results for our clients.</p>
     <a href="#" class="button">Learn more about services</a>
@@ -31,32 +31,38 @@
   <div class="antialiased w-full bg-gradient-to-b flex flex-col font-sans from-black items-stretch justify-center h-screen to-gray-900 sm:flex-row sm:items-center">
     <div class="flex flex-col flex-grow items-stretch min-w-md w-full h-full sm:flex-row sm:overflow-hidden">
 
-      <div class="active bg-dark-gray cursor-pointer duration-700 ease-in-out flex-grow min-h-14 min-w-14 overflow-hidden pane relative transition-all">
+      <div class="pane active bg-gradient-to-r from-[#6D696A] to-[#858081] cursor-pointer duration-700 ease-in-out flex-grow min-h-14 min-w-14 overflow-hidden relative transition-all">
         <div class="content flex flex-col justify-center leading-tight text-white whitespace-pre max-w-0">
-          <div class="ease-in-out font-bold duration-700 opacity-0 relative transform transition-all">Imagine</div>
-          <div class="delay-100 duration-700 ease-in-out opacity-0 relative transform transition-all">Three decades in the business allows me to provide an unfiltered perspective. </div>
+          <div class="ease-in-out font-bold duration-700 opacity-0 relative transform transition-all serif text-9xl text-dark-gray">Three decades in the business allows me to provide an unfiltered perspective.</div>
         </div>
-        <div class="absolute bottom-0 duration-700 ease-in-out flex label right-0 mb-2 ml-3 sm:mb-3 sm:ml-2 transition-all z-30">
+        <div class="absolute top-14 right-10 [writing-mode:vertical-lr] uppercase text-3xl text-white">
+          <span class="text-2xs">01</span> Investments
+        </div>
+        <div class="icon absolute bottom-0 duration-700 ease-in-out flex label right-0 mb-2 ml-3 sm:mb-3 sm:ml-2 transition-all z-30">
           <div class="bg-gray-900 flex items-center justify-center mr-3 rounded-full text-red-500">@svg('images/icon-arrows')</div>
         </div>
       </div>
 
-      <div class="bg-medium-gray cursor-pointer duration-700 ease-in-out flex-grow min-h-14 min-w-14 overflow-hidden pane relative transition-all">
+      <div class="pane bg-medium-gray cursor-pointer duration-700 ease-in-out flex-grow min-h-14 min-w-14 overflow-hidden relative transition-all">
         <div class="content flex flex-col justify-center leading-tight text-white whitespace-pre max-w-0">
-          <div class="ease-in-out font-bold duration-700 opacity-0 relative transform transition-all">Build</div>
-          <div class="delay-100 duration-700 ease-in-out opacity-0 relative transform transition-all">Realize your vision</div>
+          <div class="ease-in-out font-bold duration-700 opacity-0 relative transform transition-all serif text-9xl text-dark-gray">Realize your vision</div>
         </div>
-        <div class="absolute bottom-0 duration-700 ease-in-out flex label right-0 mb-2 ml-3 sm:mb-3 sm:ml-2 transition-all z-30">
-          <div class="bg-gray-900 flex items-center justify-center mr-3 rounded-full text-yellow-500">@svg('images/icon-arrow')</div>
+        <div class="absolute top-14 right-10 [writing-mode:vertical-lr] uppercase text-3xl text-white">
+          <span class="text-2xs">02</span> Asset & Portfolio Management
+        </div>
+        <div class="icon absolute bottom-0 right-0 duration-700 ease-in-out flex label mb-2 ml-3 sm:mb-3 sm:ml-2 transition-all z-30">
+          <div class="bg-gray-900 flex items-center justify-center rounded-full text-yellow-500">@svg('images/icon-arrow')</div>
         </div>
       </div>
 
-      <div class="bg-light-gray cursor-pointer duration-700 ease-in-out flex-grow min-h-14 min-w-14 overflow-hidden pane relative transition-all">
+      <div class="pane bg-dust cursor-pointer duration-700 ease-in-out flex-grow min-h-14 min-w-14 overflow-hidden relative transition-all">
         <div class="content flex flex-col justify-center leading-tight text-white whitespace-pre max-w-0">
-          <div class="ease-in-out font-bold duration-700 opacity-0 relative transform transition-all">Explore</div>
-          <div class="delay-100 duration-700 ease-in-out opacity-0 relative transform transition-all">Discover the world</div>
+          <div class="ease-in-out font-bold duration-700 opacity-0 relative transform transition-all serif text-9xl text-dark-gray">Discover the world</div>
         </div>
-        <div class="absolute bottom-0 duration-700 ease-in-out flex label right-0 mb-2 ml-3 sm:mb-3 sm:ml-2 transition-all z-30">
+        <div class="absolute top-14 right-10 [writing-mode:vertical-lr] uppercase text-3xl text-white">
+          <span class="text-2xs">03</span> Advisory
+        </div>
+        <div class="icon absolute bottom-0 duration-700 ease-in-out flex label right-0 mb-2 ml-3 sm:mb-3 sm:ml-2 transition-all z-30">
           <div class="bg-gray-900 flex items-center justify-center mr-3 rounded-full text-green-500">@svg('images/icon-reticle')</div>
         </div>
       </div>
