4b61ece020eb871343453af610791fed46516604
Codi Lechasseur <codilechasseur@gmail.com>
2024-02-19 10:28:55 -0800

    Privacy policy styles.

--- web/app/themes/integral/resources/views/partials/content-page-basic.blade.php
+++ web/app/themes/integral/resources/views/partials/content-page-basic.blade.php
@@ -1,4 +1,4 @@
-<div class="px-4 md:px-8 lg:px-16 bg-light-gray pt-[188px] md:pt-[325px]">
+<div class="main-content px-4 md:px-8 lg:px-16 bg-light-gray pt-[188px] md:pt-[325px] pb-12">
   <h1 class="text-9xl md:text-10xl lg:text-12xl text-charcoal font-serif">@php(the_title())</h1>
 
   @php(the_content())
